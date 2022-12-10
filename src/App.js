9ef592a21e17bb58b10debfc6605681945825a44
netox222 <souparnamahato146@gmail.com>
2022-12-10 22:28:44 +0530

    asc and dsc oder set and also change row color done

--- src/App.js
+++ src/App.js
@@ -1,130 +1,157 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 
 function App() {
 
- const [data , setData] = useState([])
+ const [uerData , setUserData] = useState([])
  const [isAscd , setIsAscd] = useState(true)
+ const [color , setClolor] = useState(true)
+const ref = useRef(null)
+
 
  useEffect(()=>{
-  fetch('http://localhost:5000/users')
+  fetch(`http://localhost:5000/users?oder=${isAscd  ? "asc" : "dsc"}`)
   .then(res => res.json())
   .then(data =>{
     console.log(data)
-    setData(data.Sheet1)
+    setUserData(data)
   })
- } , [])
-console.log(data);
+ } , [isAscd])
+console.log(uerData);
+console.log(isAscd);
+console.log(color);
+const testFunction = (id) =>{
+ const parent =   ref.current.parentElement.children[id - 1 ];
+ parent.style.backgroundColor  = "pink"
+}
+
 
   return (
     <div className="">
      <h1 className="text-center text-2xl">Table</h1>
-<div class="overflow-x-auto mt-9 relative shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+     <button onClick={()=> setIsAscd(true)} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Asc</button>
+     <button onClick={()=> setIsAscd(false)} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Dsc</button>
+<div className="overflow-x-auto mt-9 relative shadow-md sm:rounded-lg">
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        
             <tr>
-                <th scope="col" class="py-3 px-6">
+               
+                
+                
+              
+                <th scope="col" className="py-3 px-6">
                   Id
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                    First Name
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Last Name
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                    Email
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                    Gender
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                     Ip address
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                     Airport Code
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                    Time
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                     Status
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                    Mobile
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                    Area
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Show
                 </th>
-                <th scope="col" class="py-3 px-6">
+                <th scope="col" className="py-3 px-6">
                   Edit
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Change Color
                 </th>
             </tr>
         </thead>
         <tbody>
-            {/* <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            {/* <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     Apple MacBook Pro 17"
                 </th>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                     Sliver
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                     Laptop
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                     $2999
                 </td>
-                <td class="py-4 px-6">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                <td className="py-4 px-6">
+                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                 </td>
             </tr> */}
             
-        {data.map(user => {
-          return <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+        {uerData?.map(user => {
+          return <tr ref={ref} className={`  ${color ? "bg-white " : "bg-green-500"}   border-b dark:bg-gray-900 dark:border-gray-700`}>
+                
+                
+               
+                <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {user.id}
-                </th>
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                </td>
+                <td  className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {user.first_name}
-                </th>
-                <td class="py-4 px-6">
+                </td>
+                <td className="py-4 px-6 ">
                     {user.last_name}
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                     {user.email}
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                    {user.gender}
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                    {user.ip_address}
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                    {user.airport_code}
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                    {user.time}
                 </td>
                 <td className="py-4 px-6">
                    <p className={user.status === "true" ? "text-green-700" : "text-red-800" }>{user.status} </p> 
                                  
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                    {user.mobile}
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                    {user.area}
                 </td>
-                <td class="py-4 px-6">
+                <td className="py-4 px-6">
                    {user.show}
                 </td>
-                <td class="py-4 px-6">
+               
+                <td className="py-4 px-6">
                    {user.edit}
+                </td>
+                <td className="py-4 px-6">
+                   <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={()=>testFunction(user.id)}>Change Color</button>
                 </td>
                 
             </tr>
